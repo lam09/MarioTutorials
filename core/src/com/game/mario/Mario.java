33894cf6bee9e4be9e91e4b55a73a9955ea64a03
lam09 <tuananhvn27@yahoo.com>
2016-06-19 22:28:54 +0200

    sss

--- core/src/com/game/mario/Mario.java
+++ core/src/com/game/mario/Mario.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.game.mario.screens.PlayScreen;
 
 public class Mario extends Game {
 
@@ -19,9 +20,6 @@ public class Mario extends Game {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.end();
+		super.render();
 	}
 }
--- core/src/com/game/mario/screens/PlayScreen.java
+++ core/src/com/game/mario/screens/PlayScreen.java
@@ -2,7 +2,13 @@ package com.game.mario.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.game.mario.Mario;
 
 /**
@@ -11,10 +17,16 @@ import com.game.mario.Mario;
 public class PlayScreen implements Screen {
     private Mario game;
     Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
     public PlayScreen(Mario game)
     {
         this.game = game;
-        texture = new Texture("")
+        texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+   //     gamePort = new StretchViewport(800, 480 , gameCam);
+//        gamePort = new ScreenViewport(gameCam);
+        gamePort = new FitViewport(800,480,gameCam);
     }
 
     @Override
@@ -24,14 +36,17 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
         Gdx.gl.glClearColor(1,0,0,1);
-        Gdx.gl.glClear();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        game.batch.draw(texture,0,0);
+        game.batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width,height);
     }
 
     @Override
